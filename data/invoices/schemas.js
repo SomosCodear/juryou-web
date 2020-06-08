48eb8034e125d8cb31d0e093425c228edb755f16
Agustin Carrasco <asermax@gmail.com>
2020-06-08 01:31:18 -0300

    feat(invoices): put together validation schema for invoice

---
+++ data/invoices/schemas.js
@@ -0,0 +1,23 @@
+import * as yup from 'yup';
+
+const emailSchema = yup.object().shape({
+  sender: yup.string().email().required(),
+  subject: yup.string().required(),
+  body: yup.string().required(),
+  attachmentName: yup.string().required(),
+});
+const customerSchema = yup.object().shape({
+  identityDocument: yup.string().matches(/^\d+$/).required(),
+  name: yup.string().required(),
+  email: yup.string().email().required(),
+});
+const itemSchema = yup.object().shape({
+  name: yup.string().required(),
+  amount: yup.number().integer().moreThan(0),
+  price: yup.string().matches(/^\d+\.?\d*$/),
+});
+export const invoiceSchema = yup.object().shape({
+  email: emailSchema.required(),
+  customer: customerSchema.required(),
+  items: yup.array().of(itemSchema).min(1).required(),
+});
